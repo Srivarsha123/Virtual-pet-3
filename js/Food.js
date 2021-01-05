5ffbf4ad19338d7ebd777616cbb789d6bafd208d
Srivarsha123 <73740042+Srivarsha123@users.noreply.github.com>
2021-01-05 18:33:59 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,108 @@
+class Foods{
+    constructor(){
+        this.image = loadImage("images/Milk.png")
+        this.addFood = createButton("Add Food");
+        this.feedDog = createButton("Feed the dog");
+        //this.foodCount = 0;
+    }
+
+    getState(){
+      var gameStateRef  = database.ref('gameState');
+      gameStateRef.on("value",function(data){
+         gameState = data.val();
+         //console.log("Hi")
+      })
+  
+    }
+  
+    update(state){
+      database.ref('/').update({
+        gameState: state
+      });
+    }
+
+    addingFood(count){
+      database.ref('/').update({
+      Food:count
+       })
+      //console.log(count)
+      }
+
+    display(){
+      var x=10,y=50;
+        if(foodS!=0){
+            for(var i=0;i<foodS;i++){
+                 if(i%10==0){
+                       x=10;
+                       y=y+90;
+                  }
+            image(this.image,x,y,90,90);
+            x=x+30;
+            }
+        }
+
+      //console.log(gameState)
+      this.addFood.position(360, 55);
+      this.addFood.mousePressed(()=> {
+        foodCount +=1
+        foodObj.addingFood(foodCount)
+      });
+      this.addFood.size(100,35)
+      this.addFood.style('font-size', '15px');
+      this.addFood.style('font-color', 'white');
+      this.addFood.style('border-radius', '100%')
+      this.addFood.style('background-color','color(25, 23, 200, 50)')
+      this.addFood.style("font-family","Comic Sans MS");
+
+      this.feedDog.position(780, 55);
+      if(foodS != 0){
+      this.feedDog.mousePressed(()=> {
+        foodObj.writeStock(foodS)
+      });
+      }
+      this.feedDog.size(110,35)
+      this.feedDog.style('font-size', '15px');
+      this.feedDog.style('border-radius', '100%');
+      this.feedDog.style('background-color','color(25, 23, 200, 50)');
+      this.feedDog.style("font-family","Comic Sans MS");
+    }
+
+  
+  writeStock(x){
+    if(x<=0){
+      x = 0
+    }
+    else{
+      x = x - 1
+    }
+
+    database.ref('/').update({
+       Food:x,
+       lastFed:hour()
+    })
+  }
+
+  bedroom(){
+    background(bedroom);
+  }
+
+  washroom(){
+    background(washroom);
+  }
+
+  garden(){
+    background(garden);
+  }
+
+  
+  hide(){
+    this.addFood.hide();
+    this.feedDog.hide();
+  }
+
+  show(){
+    this.addFood.show();
+    this.feedDog.show();
+  }
+
+}
---
+++ sketch.js
@@ -0,0 +1,117 @@
+var  dog, happyDog, database, foodS, foodStock, dogImg, happyDogImg, milk, milkImg, bgImg;
+var addFood, feedDog, foodObj, lastfed, feed;
+var foodCount = 0;
+var gameState;
+var bedroom, garden, washroom, sad;
+
+function preload(){
+  //dogImg = loadImage("images/dogImg.png");
+  //happyDogImg = loadImage("images/dogImg1.png");
+  milkImg = loadImage("images/Milk.png");
+  bgImg = loadImage("images/bg.png");
+  bedroom = loadImage("images/Bed Room.png");
+  garden = loadImage("images/Garden.png");
+  washroom = loadImage("images/Wash Room.png");
+  sad = loadImage("images/vaccination.png");
+}
+
+function setup() {
+  foodObj = new Foods();
+  createCanvas(550, 500);
+
+  database = firebase.database();
+  
+  dog = createSprite(425, 400);
+  dog.addImage(sad);
+  dog.scale = 0.3;
+
+  foodStock = database.ref('Food');
+  foodStock.on("value",readStock);
+
+}
+
+
+function draw() {  
+  background(bgImg);
+
+  foodObj.display();
+
+  textSize(20)
+  fill("black")
+  stroke(3)
+
+  if(foodS !== 0){
+    dog.addImage(sad)
+    dog.scale = 0.3;
+  }
+  else{
+    dog.scale = 0.3;
+  }
+
+  if(foodS === undefined){
+    foodS = 0
+  }
+
+  if(gameState === "hungry"){
+    foodObj.show();
+    dog.visible = true;
+    text("Food Remaining: "+foodS, 190, 50);
+  }
+  else if(gameState != "hungry"){
+    foodObj.hide();
+    dog.visible = false;
+  }
+
+  if(hour()===lastfed+1){
+    foodObj.garden();
+    foodObj.update("playing")
+  }
+
+  else if(hour()===lastfed+2){
+    foodObj.bedroom();
+    foodObj.update("sleeping")
+  }
+
+  else if(hour()===lastfed+3){
+    foodObj.washroom();
+    foodObj.update("bathing")
+  }
+
+  else{
+  foodObj.update("hungry")
+  }
+
+  feed = database.ref('lastFed')
+  feed.on("value", (data)=>{
+    lastfed = data.val();
+  })
+  if(lastfed!=undefined){
+  if(lastfed>12){
+    text("Last Feed: "+lastfed%12 + " PM", 200, 80);
+  }
+  else if(lastfed===0){
+    text("Last Feed: 12 AM", 200, 80);
+  }
+
+  else if(lastfed===12){
+    text("Last Feed: 12 PM", 200, 80);
+  }
+  else{
+    text("Last Feed: "+lastfed + " AM", 200, 80);
+  }}
+
+  drawSprites();
+  foodObj.getState();
+  foodCount = foodS
+
+  //console.log(gameState);
+}
+
+
+function readStock(data){
+  foodS = data.val();
+}
+
+
+
+
